f2f513386154070720af403adefe7324b5485a9c
darwishmammo <34026849+darwishmammo@users.noreply.github.com>
2019-08-20 12:40:48 +0200

    add database and create endpoint for logging both infos and errors

--- api/logger.js
+++ api/logger.js
@@ -5,6 +5,7 @@ const express = require("express");
 const router = express.Router();
 const appRoot = require("app-root-path");
 const fs = require("fs");
+const db = require("./database");
 
 /**
  * Logger
@@ -15,30 +16,45 @@ const logger = require("../currentImplementation/winstonLogger");
  * Router Endpoints
  */
 
-router.post("/log-error", (req, res) => {
+router.post("/log-error", async (req, res) => {
   const message = req.body;
+  await db.execute("insert into logs SET message = ?, level = 'error'", [
+    JSON.stringify(message)
+  ]);
   logger.error(message);
-  /**
-   * We don't really need a JSON response,
-   * a status code of 200 OK is enough
-   * But as it is now it will respond with 200 OK even if it crashed
-   */
   res.status(200).json();
 });
 
-router.post("/log-info", (req, res) => {
+router.post("/log-info", async (req, res) => {
   const message = req.body;
+  await db.execute("insert into logs SET message = ?, level = 'info'", [
+    JSON.stringify(message)
+  ]);
   logger.info(message);
   res.status(200).json();
 });
 
 router.get("/log-info", async (req, res) => {
-  const logs = await fs.readFile(`${appRoot}/logs/info.log`, (error, data) => {
-    if (error) {
-      return res.status(500).json({ msg: "No logs could be found!" });
-    }
-    res.send(data);
-  });
+  // const logs = await fs.readFile(`${appRoot}/logs/info.log`, (error, data) => {
+  //   if (error) {
+  //     return res.status(500).json({ msg: "No logs could be found!" });
+  //   }
+
+  //   const array = data.toString().split("\n");
+  //   db.execute("select * from logs").then(ss => console.log(ss[0][0]));
+  //   array.forEach(object => console.log(JSON.parse(object)));
+  //   res.send(array);
+  // });
+
+  const logs = await db
+    .query("select * from logs where level = 'info'")
+    .then(results => res.send(results[0]));
+});
+
+router.get("/log-error", async (req, res) => {
+  const logs = await db
+    .query("select * from logs where level = 'error'")
+    .then(results => res.send(results[0]));
 });
 
 /**
--- currentImplementation/winstonLogger.js
+++ currentImplementation/winstonLogger.js
@@ -2,42 +2,44 @@ const winston = require("winston");
 const appRoot = require("app-root-path");
 
 const logConfiguration = {
-	transports: [
-		/**
-		 * We need to also see live stuff so I've added a Console transport here...
-		 */
-		new winston.transports.Console({
-			format: winston.format.combine(
-				winston.format.timestamp(),
-				winston.format.printf((info) => {
-					return `${info.timestamp} - [${info.level}]: ${JSON.stringify(info.message)}`;
-				}))
-		}),
+  transports: [
+    /**
+     * We need to also see live stuff so I've added a Console transport here...
+     */
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(info => {
+          return `${info.timestamp} - [${info.level}]: ${JSON.stringify(
+            info.message
+          )}`;
+        })
+      )
+    }),
 
-		new winston.transports.File({
-			level: "error",
-			filename: `${appRoot}/logs/errors.log`,
-			maxsize: 5242880,
-			maxFiles: 5,
-			colorize: false,
-			format: winston.format.combine(
-				winston.format.timestamp(),
-				winston.format.json()
-			)
-		}),
-		new winston.transports.File({
-			level: "info",
-			filename: `${appRoot}/logs/info.log`,
-			maxsize: 5242880,
-			maxFiles: 5,
-			colorize: false,
-			format: winston.format.combine(
-				winston.format.timestamp(),
-				winston.format.json()
-			)
-		})
-
-	]
+    new winston.transports.File({
+      level: "error",
+      filename: `${appRoot}/logs/errors.log`,
+      maxsize: 5242880,
+      maxFiles: 5,
+      colorize: false,
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      )
+    }),
+    new winston.transports.File({
+      level: "info",
+      filename: `${appRoot}/logs/info.log`,
+      maxsize: 5242880,
+      maxFiles: 5,
+      colorize: false,
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      )
+    })
+  ]
 };
 
 const logger = winston.createLogger(logConfiguration);
