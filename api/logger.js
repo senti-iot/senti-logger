5e041752eac78adfdb32e25201e0ff098c4ffd43
darwishmammo <34026849+darwishmammo@users.noreply.github.com>
2019-08-30 09:15:58 +0200

    add endpoint to get all logs

--- api/logger.js
+++ api/logger.js
@@ -3,11 +3,8 @@
  */
 const express = require("express");
 const router = express.Router();
-// const appRoot = require("app-root-path");
-// const fs = require("fs");
-// const db = require("./database");
-const moment = require('moment')
-const mysqlFormat = 'YYYY-MM-DD HH:mm:ss'
+const moment = require("moment");
+const mysqlFormat = "YYYY-MM-DD HH:mm:ss";
 /**
  * Logger
  */
@@ -16,7 +13,7 @@ const logger = require("../currentImplementation/winstonLogger");
 /**
  * MySQL connection
  */
-const mysqlConn = require("../api/mysqlConn")
+const mysqlConn = require("../api/mysqlConn");
 
 /**
  * MySQL Queries
@@ -27,9 +24,9 @@ const insertErrorQuery = `INSERT INTO logs
 							level= "error",
 							timestamp=NOW(),
 							origin=?
-						`
+						`;
 const getErrorsQuery = `SELECT * from logs
-						WHERE timestamp >= ? and timestamp <= ? AND level = "error"`
+						WHERE timestamp >= ? and timestamp <= ? AND level = "error"`;
 
 const insertInfoQuery = `INSERT INTO logs
 						SET
@@ -37,107 +34,88 @@ const insertInfoQuery = `INSERT INTO logs
 						level="info",
 						timestamp=NOW(),
 						origin=?
-						`
+						`;
 
 const getInfosQuery = `SELECT * from logs
-					  WHERE timestamp >= ? and timestamp <= ? AND level = "info"`
+					  WHERE timestamp >= ? and timestamp <= ? AND level = "info"`;
 
+const getAllQuery = `SELECT * from logs WHERE timestamp >= ? and timestamp <= ?`;
 /**
  * Router Endpoints
  */
 
 router.post("/log-error", async (req, res) => {
-	let message = JSON.stringify(req.body.message);
-	let origin = req.body.origin
-	// await db.execute("insert into logs SET message = ?, level = 'error'", [
-	// 		JSON.stringify(message)
-	// ]);
+  let message = JSON.stringify(req.body.message);
+  let origin = req.body.origin;
 
-	await mysqlConn.query(insertErrorQuery, [message, origin]).then(rs => {
-		if (rs[0].insertId > 0) {
-			res.status(200).json()
-		}
-		else {
-			res.status(500).json()
-			logger.error('Logger failed to store message in DB')
-		}
+  await mysqlConn.query(insertErrorQuery, [message, origin]).then(rs => {
+    if (rs[0].insertId > 0) {
+      res.status(200).json();
+    } else {
+      res.status(500).json();
+      logger.error("Logger failed to store message in DB");
+    }
+  });
 
-	})
-	// We still store the message on the files
-	// res.status(200).json();
-	logger.error(message);
+  logger.error(message);
 });
 
 router.post("/log-info", async (req, res) => {
-	let message = JSON.stringify(req.body.message);
-	let origin = req.body.origin
+  let message = JSON.stringify(req.body.message);
+  let origin = req.body.origin;
 
-	// await db.execute("insert into logs SET message = ?, level = 'info'", [
-	// 	JSON.stringify(message)
-	// ]);
-	await mysqlConn.query(insertInfoQuery, [message, origin]).then(rs => {
-		if (rs[0].insertId > 0) {
-			res.status(200).json()
-		}
-		else {
-			res.status(500).json()
-			logger.error('Logger failed to store message in DB')
-		}
-	}).catch(err => {
-		console.log(err)
-		logger.error('Logger failed to store message in DB')
+  await mysqlConn
+    .query(insertInfoQuery, [message, origin])
+    .then(rs => {
+      if (rs[0].insertId > 0) {
+        res.status(200).json();
+      } else {
+        res.status(500).json();
+        logger.error("Logger failed to store message in DB");
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      logger.error("Logger failed to store message in DB");
+    });
 
-	})
-	logger.info(message);
-	// res.status(200).json();
+  logger.info(message);
 });
 
 router.get("/log-info/:from/:to", async (req, res) => {
-	// const logs = await fs.readFile(`${appRoot}/logs/info.log`, (error, data) => {
-	//   if (error) {
-	//     return res.status(500).json({ msg: "No logs could be found!" });
-	//   }
+  const startDate = moment(req.params.from).format(mysqlFormat);
+  const endDate = moment(req.params.to).format(mysqlFormat);
 
-	//   const array = data.toString().split("\n");
-	//   db.execute("select * from logs").then(ss => console.log(ss[0][0]));
-	//   array.forEach(object => console.log(JSON.parse(object)));
-	//   res.send(array);
-	// });
+  await mysqlConn
+    .query(getInfosQuery, [startDate, endDate])
+    .then(rs => {
+      res.status(200).json(rs[0]);
+    })
+    .catch(err => console.log(err));
+});
 
-	/**
-	 * What if we have 2 milion logs dating from 4 years ago? Could a poor Intel i3 with 4gb RAM handle all of them?
-	 * We need a filter, a date time one is perfect for this
-	 */
-
-	const startDate = moment(req.params.from).format(mysqlFormat)
-	const endDate = moment(req.params.to).format(mysqlFormat)
-
-
-	await mysqlConn.query(getInfosQuery, [startDate, endDate]).then(rs => {
-		res.status(200).json(rs[0])
-	}).catch(err => console.log(err))
-
-	/**
-	 * Unused variables are a no no...
-	 */
-	// const logs = await db
-	// 	.query("select * from logs where level = 'info'")
-	// 	.then(results => res.send(results[0]));
+router.get("/logs/:from/:to", async (req, res) => {
+  const startDate = moment(req.params.from).format(mysqlFormat);
+  const endDate = moment(req.params.to).format(mysqlFormat);
+  console.log(getAllQuery, startDate, endDate);
+  await mysqlConn
+    .query(getAllQuery, [startDate, endDate])
+    .then(rs => {
+      res.status(200).json(rs[0]);
+    })
+    .catch(err => console.log(err));
 });
 
 router.get("/log-error/:from/:to", async (req, res) => {
+  const startDate = moment(req.params.from).format(mysqlFormat);
+  const endDate = moment(req.params.to).format(mysqlFormat);
 
-	const startDate = moment(req.params.from).format(mysqlFormat)
-	const endDate = moment(req.params.to).format(mysqlFormat)
-
-
-	await mysqlConn.query(getErrorsQuery, [startDate, endDate]).then(rs => {
-		res.status(200).json(rs[0])
-	}).catch(err => console.log(err))
-
-	// const logs = await db
-	// 	.query("select * from logs where level = 'error'")
-	// 	.then(results => res.send(results[0]));
+  await mysqlConn
+    .query(getErrorsQuery, [startDate, endDate])
+    .then(rs => {
+      res.status(200).json(rs[0]);
+    })
+    .catch(err => console.log(err));
 });
 
 /**
